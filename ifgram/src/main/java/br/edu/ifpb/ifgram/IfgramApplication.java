0f5fd0f928ac8c8bbe40a9861f6de7b6b3bd0dc2
Monik Vitórya Silva Barbosa <202412060006@ad.ifpb.edu.br>
2026-08-27 08:34:27 -0300

    primeiro commit

---
+++ ifgram/src/main/java/br/edu/ifpb/ifgram/IfgramApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.ifgram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfgramApplication.class, args);
+	}
+
+}
---
+++ ifgram/src/test/java/br/edu/ifpb/ifgram/IfgramApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.ifgram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

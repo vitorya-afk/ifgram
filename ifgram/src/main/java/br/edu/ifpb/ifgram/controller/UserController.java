493b0109ff7d478e79056d0f3ce51d2321e7a139
Monik Vitórya Silva Barbosa <202412060006@ad.ifpb.edu.br>
2026-09-10 07:57:37 -0300

    endpoints

---
+++ ifgram/src/main/java/br/edu/ifpb/ifgram/controller/UserController.java
@@ -0,0 +1,32 @@
+package br.edu.ifpb.ifgram.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("Monik")
+public class UserController {
+
+    @GetMapping
+    public String getUser(){
+        return "get user was called";
+    }
+    @PostMapping
+    public String postUser(){
+        return "Chamei o endpoint como um post";
+    }
+    @PutMapping
+    public String putUser(){
+        return "Chamei o endpoint como um put";
+    }
+    @PatchMapping
+    public String patchUser(){
+        return "Chamei o endpoint como um patch";
+    }
+    @DeleteMapping
+    public String deletar(){
+        return null;
+    }
+
+
+
+}
